5ee4a2a4da387a74e52f26b8eabdb6ce7a90ff86
anerdsmeltybrain <hdviews.01@gmail.com>
2026-05-01 20:17:08 -0400

    added the game structure

--- jungleRun/jungleRun.c
+++ jungleRun/jungleRun.c
@@ -104,6 +104,13 @@ struct itemList {
   struct Item * items;
 };
 
+struct game {
+  struct entityList * players;
+  struct entityList * mobs;
+  struct entityList * blocks;
+  struct itemList * items;
+};
+
 //Entity Functions
 void initEntity(struct Entity *, enum entityType, Vector2);
 void initEntityPlayer(struct Entity *, enum entityType, enum playerType, Vector2);
@@ -114,6 +121,18 @@ void drawEntity(struct Entity *);
 
 //Entity List Function
 void reallocEntityList(struct entityList *);
+
+//Game Struct Functions
+void initGamePlayers(struct game *, int, int);
+void initGameMobs(struct game *, int, int);
+void initGameBlocks(struct game *, int, int);
+void initGameItems(struct game *, int, int);
+void initGame(struct game *, int, int);
+void addGamePlayer(struct game *, struct entityList *, enum entityType, enum playerType, Vector2);
+void addGameMob(struct game *, struct entityList *, enum entityType, enum mobType, Vector2);
+void addGameBlock(struct game *, struct entityList *, enum entityType, enum blockType, Vector2);
+void addGameBlockSpawner(struct game *, struct entityList *, enum entityType, enum blockType, enum mobType, Vector2);
+void drawGame(struct game *);
 
 //Item List Function
 void initItem(struct Item *, enum itemType, Vector2); 
@@ -129,7 +148,7 @@ void mobPhysics(struct Entity *, struct entityList *);
 void mobAnimations(struct Entity *);
 
 //Mob Spawner Functions
-void mobSpawnerProcess(struct Entity *);
+void mobSpawnerProcess(struct Entity *, struct game *, enum entityType, enum mobType, Vector2);
 
 //Block Functions
 void blockWeightChecker(struct Entity *, struct Entity *, int);
@@ -143,56 +162,47 @@ int main() {
   InitWindow(screenWidth, screenHeight, "Jungle Run");
   SetTargetFPS(60);
 
+  //the whole damn game
+  struct game mainGame;
+  initGame(&mainGame, 5, 0);
+
   //all things player
   /* ---------------------------------------------------------- */
-  struct Entity player;
-  initEntityPlayer(&player, PLAYER, RANGER, (Vector2){0, 16});
+  addGamePlayer(&mainGame, mainGame.players, PLAYER, MAGE, (Vector2){0, 16});
   /* ---------------------------------------------------------- */
 
   //all things mobs
   /* ---------------------------------------------------------- */
-  struct Entity slimeSpawner;
-  initEntityBlockSpawner(&slimeSpawner, BLOCK, MOBSPAWNER, SLIME, (Vector2){96, 48});
   /* ---------------------------------------------------------- */
 
 
-  // all the grass on the screen
+  // all things blocks
   /* ---------------------------------------------------------- */
-  struct entityList grassBlocks;
-  grassBlocks.capacity = 1550;
-  grassBlocks.counter = 0;
-  grassBlocks.ents = malloc(grassBlocks.capacity * sizeof (struct Entity));
-
   for(int i = 0; i < 25; i++) {
-    for(int j = 0; j < 3; j++) {
-       initEntity(&grassBlocks.ents[grassBlocks.counter], BLOCK, (Vector2){i * 16, j * 16 + 64});
-       grassBlocks.counter++;
+    for(int j = 0; j < 4; j++) {
+      addGameBlock(&mainGame, mainGame.blocks, BLOCK, GRASS, (Vector2){i * 16, j * 16 + 48});
     }
   }
 
-  for(int i = 0; i < grassBlocks.counter; i++) {
-    blockWeightChecker(&grassBlocks.ents[i], grassBlocks.ents, grassBlocks.capacity);
-    blockSourceDestChecker(&grassBlocks.ents[i]);
+  for(int i = 0; i < mainGame.blocks->counter; i++) {
+    blockWeightChecker(&mainGame.blocks->ents[i], mainGame.blocks->ents, mainGame.blocks->counter);
+    blockSourceDestChecker(&mainGame.blocks->ents[i]);
   }
+
+  addGameBlockSpawner(&mainGame, mainGame.blocks, BLOCK, MOBSPAWNER, SLIME, (Vector2){32, 32});
   /* ---------------------------------------------------------- */
   
   //camera functionalities
   Camera2D camera = { 0 };
   camera.offset = (Vector2){screenWidth / 2, screenHeight / 2};
-  camera.target = (Vector2){player.destRect.x + 8, player.destRect.y + 8};
+  camera.target = (Vector2){mainGame.players->ents[0].destRect.x + 8, mainGame.players->ents[0].destRect.y + 8};
   camera.rotation = 0.0f;
   camera.zoom = 2.0f;
 
   while(!WindowShouldClose()) {
-    camera.target = (Vector2){player.destRect.x + 8, player.destRect.y + 8};
-    playerPhysics(&player, &grassBlocks);
-    playerControls(&player);
-
-    mobSpawnerProcess(&slimeSpawner);
-    for(int i = 0; i < slimeSpawner.b.mobSpawnerProps.mobs.counter; i++) {
-      mobAnimations(&slimeSpawner.b.mobSpawnerProps.mobs.ents[i]);
-      mobPhysics(&slimeSpawner.b.mobSpawnerProps.mobs.ents[i], &grassBlocks);
-    }
+    camera.target = (Vector2){mainGame.players->ents[0].destRect.x + 8, mainGame.players->ents[0].destRect.y + 8};
+    playerPhysics(&mainGame.players->ents[0], mainGame.blocks);
+    playerControls(&mainGame.players->ents[0]);
     
     BeginDrawing();
     ClearBackground(BLACK);
@@ -200,18 +210,8 @@ int main() {
     DrawText(TextFormat("Score: %d", playerScore), 0, 0, 32, WHITE);
     BeginMode2D(camera);
 
-      for(int i = 0; i < grassBlocks.counter; i++) {
-        // blockWeightChecker(&grassTiles[i], grassTiles, 100);
-        // blockSourceDestChecker(&grassBlocks.ents[i]);
-        drawEntity(&grassBlocks.ents[i]);
-      }
+      drawGame(&mainGame);
 
-      drawEntity(&slimeSpawner);
-      for(int i = 0; i < slimeSpawner.b.mobSpawnerProps.mobs.counter; i++) {
-        drawEntity(&slimeSpawner.b.mobSpawnerProps.mobs.ents[i]);
-      }
-
-      drawEntity(&player);
     EndMode2D();
 
     EndDrawing();
@@ -369,22 +369,22 @@ void initEntityBlockSpawner(struct Entity * ent, enum entityType et, enum blockT
     case SLIME:
       ent->sourceRect = (Rectangle){0, 0, 16, 16};
       ent->b.mobSpawnerProps.maxEntities = 5;
-      ent->b.mobSpawnerProps.timeCounter = 30;
+      ent->b.mobSpawnerProps.timeCounter = 300;
       break;
     case GOBLIN:
       ent->sourceRect = (Rectangle){16, 0, 16, 16};
       ent->b.mobSpawnerProps.maxEntities = 5;
-      ent->b.mobSpawnerProps.timeCounter = 45;
+      ent->b.mobSpawnerProps.timeCounter = 450;
       break;
     case GOLEM:
       ent->sourceRect = (Rectangle){32, 0, 16, 16};
       ent->b.mobSpawnerProps.maxEntities = 5;
-      ent->b.mobSpawnerProps.timeCounter = 60;
+      ent->b.mobSpawnerProps.timeCounter = 600;
       break;
     case BAT:
       ent->sourceRect = (Rectangle){48, 0, 16, 16};
       ent->b.mobSpawnerProps.maxEntities = 5;
-      ent->b.mobSpawnerProps.timeCounter = 30;
+      ent->b.mobSpawnerProps.timeCounter = 300;
       break;
   }
 }
@@ -393,9 +393,121 @@ void drawEntity(struct Entity * ent) {
 
   if(ent->isActive) {
     DrawTexturePro(ent->texture, ent->sourceRect, ent->destRect, (Vector2){0.0, 0.0}, 0.0f, ent->color);
-    if(ent->et == BLOCK)
-      DrawText(TextFormat("%d", ent->b.blockWeight), ent->destRect.x, ent->destRect.y, 8, WHITE); 
+    // if(ent->et == BLOCK)
+    //   DrawText(TextFormat("%d", ent->b.blockWeight), ent->destRect.x, ent->destRect.y, 8, WHITE); 
   }
+}
+
+void initGamePlayers(struct game * gm, int cap, int count) {
+  gm->players = malloc(sizeof(struct entityList));
+  gm->players->capacity = cap;
+  gm->players->counter = count;
+  gm->players->ents = malloc(sizeof (struct Entity));
+}
+
+void initGameMobs(struct game * gm, int cap, int count) {
+  gm->mobs = malloc(sizeof(struct entityList));
+  gm->mobs->capacity = cap;
+  gm->mobs->counter = count;
+  gm->mobs->ents = malloc(sizeof (struct Entity));
+}
+
+void initGameBlocks(struct game * gm, int cap, int count) {
+  gm->blocks = malloc(sizeof(struct entityList));
+  gm->blocks->capacity = cap;
+  gm->blocks->counter = count;
+  gm->blocks->ents = malloc(sizeof (struct Entity));
+}
+
+void initGameItems(struct game * gm, int cap, int count) {
+  gm->items = malloc(sizeof(struct itemList));
+  gm->items->capacity = cap;
+  gm->items->counter = count;
+  gm->items->items = malloc(sizeof (struct Item));
+}
+
+void initGame(struct game * gm, int cap, int count) {
+  initGamePlayers(gm, cap, count);
+  initGameMobs(gm, cap, count);
+  initGameBlocks(gm, cap, count);
+  initGameItems(gm, cap, count);
+}
+
+void addGamePlayer(struct game * gm, struct entityList * el, enum entityType et, enum playerType pt, Vector2 pos) {
+  if (gm->players->counter == 1) {
+    // gm->players->ents = realloc(gm->players->ents, sizeof (struct Entity));
+    initEntityPlayer(&gm->players->ents[gm->players->counter], et, pt, pos);
+    gm->players->counter++;
+  } else {
+    gm->players->capacity++;
+    gm->players->ents = realloc(gm->players->ents, gm->players->capacity * sizeof (struct Entity));
+    initEntityPlayer(&gm->players->ents[gm->players->counter], et, pt, pos);
+    gm->players->counter++;
+  }
+}
+
+void addGameMob(struct game * gm, struct entityList * el, enum entityType et, enum mobType mt, Vector2 pos) {
+  if (gm->players->counter == 1) {
+    // gm->players->ents = realloc(gm->players->ents, sizeof (struct Entity));
+    initEntityMob(&gm->players->ents[gm->players->counter], et, mt, pos);
+    gm->players->counter++;
+  } else {
+    gm->players->capacity++;
+    // gm->players->ents = realloc(gm->players->ents, gm->players->capacity * sizeof (struct Entity));
+    reallocEntityList(el);
+    initEntityMob(&gm->players->ents[gm->players->counter], et, mt, pos);
+    gm->players->counter++;
+  }
+  
+}
+
+void addGameBlock(struct game * gm, struct entityList * el, enum entityType et, enum blockType bt, Vector2 pos) {
+  
+  if (gm->blocks->counter == 0) {
+    // gm->blocks->ents = realloc(gm->blocks->ents, sizeof (struct Entity));
+    initEntityBlock(&gm->blocks->ents[gm->blocks->counter], et, bt, pos);
+    gm->blocks->counter++;
+  } else {
+    gm->blocks->capacity++;
+    // gm->blocks->ents = realloc(gm->blocks->ents, gm->blocks->capacity * sizeof (struct Entity));
+    reallocEntityList(gm->blocks);
+    initEntityBlock(&gm->blocks->ents[gm->blocks->counter], et, bt, pos);
+    gm->blocks->counter++;
+  }
+}
+
+void addGameBlockSpawner(struct game * gm, struct entityList * el, enum entityType et, enum blockType bt, enum mobType mt, Vector2 pos) {
+  
+  if (gm->blocks->counter == 0) {
+    // gm->blocks->ents = realloc(gm->blocks->ents, sizeof (struct Entity));
+    initEntityBlockSpawner(&gm->blocks->ents[gm->blocks->counter], et, bt, mt, pos);
+    gm->blocks->counter++;
+  } else {
+    gm->blocks->capacity++;
+    // gm->blocks->ents = realloc(gm->blocks->ents, gm->blocks->capacity * sizeof (struct Entity));
+    reallocEntityList(gm->blocks);
+    initEntityBlockSpawner(&gm->blocks->ents[gm->blocks->counter], et, bt, mt, pos);
+    gm->blocks->counter++;
+  }
+}
+
+void drawGame(struct game * gm) {
+
+  for(int i = 0; i < gm->blocks->counter; i++) {
+    drawEntity(&gm->blocks->ents[i]);
+  }
+
+  for(int i = 0; i < gm->mobs->counter; i++) {
+    drawEntity(&gm->mobs->ents[i]);
+  }
+
+  for(int i = 0; i < gm->players->counter; i++) {
+    drawEntity(&gm->players->ents[i]);
+  }
+  
+  // for(int i = 0; i < gm->items->counter; i++) {
+  //   drawItem(&gm->items->items[i]);
+  // }
 }
 
 void initItem(struct Item * item, enum itemType it, Vector2 pos) {
@@ -450,7 +562,7 @@ void playerPhysics(struct Entity * player, struct entityList * blocks) {
   
 
     for(int i = 0; i < blocks->capacity; i++) {
-      if(CheckCollisionRecs(player->destRect, blocks->ents[i].destRect)) {
+      if((CheckCollisionRecs(player->destRect, blocks->ents[i].destRect)) && (blocks->ents[i].b.bt != MOBSPAWNER)) {
         player->destRect.y -= fallSpeed;
         fallCounter = 0;
       }
@@ -506,15 +618,12 @@ void mobAnimations(struct Entity * mob) {
   i++;
 }
 
-void mobSpawnerProcess(struct Entity * ent) {
+// void addGameMob(struct game * gm, struct entityList * el, enum entityType et, enum mobType mt, Vector2 pos) 
+void mobSpawnerProcess(struct Entity * mobSpawner, struct game * gm, enum entityType et, enum mobType mt, Vector2 pos) {
   
   static int timerCount = 0;
 
-  if((timerCount % ent->b.mobSpawnerProps.timeCounter == 0) && (ent->b.mobSpawnerProps.mobs.capacity <= ent->b.mobSpawnerProps.maxEntities)) {
-    ent->b.mobSpawnerProps.mobs.capacity++;
-    ent->b.mobSpawnerProps.mobs.ents = realloc(ent->b.mobSpawnerProps.mobs.ents, ent->b.mobSpawnerProps.mobs.capacity * sizeof (struct Entity));
-    initEntityMob(&ent->b.mobSpawnerProps.mobs.ents[ent->b.mobSpawnerProps.mobs.counter], MOB, ent->b.mobSpawnerProps.mt, (Vector2){ent->destRect.x, ent->destRect.y});
-    ent->b.mobSpawnerProps.mobs.counter++;
+  if((timerCount % mobSpawner->b.mobSpawnerProps.timeCounter == 0) && (mobSpawner->b.mobSpawnerProps.mobs.capacity <= mobSpawner->b.mobSpawnerProps.maxEntities)) {
   }
 
   timerCount++;
